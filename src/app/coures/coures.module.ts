0104b8fe3137c9b69082cee6a7f9a3a803057dfe
Rahul <rahulkr.mits@gmail>
2021-02-10 10:47:13 +0530

    module added

---
+++ src/app/coures/coures.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CouresRoutingModule } from './coures-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CouresRoutingModule
+  ]
+})
+export class CouresModule { }
